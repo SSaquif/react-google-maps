949254c7f06418c5451cb944c9e77204f3ade73e
Sadnan Saquif <ssaquif@gmail.com>
2020-11-13 02:33:08 -0500

    added global styles for normalization, reduced motion and css variables

---
+++ src/Styles/CSSVariables.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const CSSVariables = createGlobalStyle`
+:root {
+  /* Font Sizes */
+  --large-screen-font: 20px;
+  --desktop-font: 16px; /* Beleive this is browser standard*/
+  --table-font: 16px;
+  --mobile-font: 12px;
+  /*  */
+
+  /* Variables created for navbar-1 navbar-dropdown-1 (fb)*/
+  /* --bg-accent: #484a4d; */
+  --nav-bg: #242526;
+  --nav-height: 60px;
+  /* --nav-button-contatiner-size: calc(var(--nav-height) * 0.6);
+  --nav-button-size: calc(var(--nav-height) * 0.4);
+  --text-color: #dadce1;
+  --border: 1px solid #dadce1;
+  --border-radius: 8px;
+  --speed: 500ms; */
+}
+`;
+
+export default CSSVariables;
---
+++ src/Styles/ReducedMotion.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const ReducedMotion = createGlobalStyle`
+@media (prefers-reduced-motion: reduce) {
+  *,
+  ::before,
+  ::after {
+    animation-delay: -1ms !important;
+    animation-duration: 1ms !important;
+    animation-iteration-count: 1 !important;
+    background-attachment: initial !important;
+    scroll-behavior: auto !important;
+    transition-delay: 0s !important;
+    transition-duration: 0s !important;
+  }
+}
+`;
+
+export default ReducedMotion;
