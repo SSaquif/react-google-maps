90763655689aa05dc02a672df4ad35177a739199
Sadnan Saquif <ssaquif@gmail.com>
2020-11-13 02:34:00 -0500

    added map styles from snazzy map

---
+++ src/Utils/MapStyles/mapWithOutLabels.js
@@ -0,0 +1,22 @@
+const mapWithoutLabels = [
+  {
+    featureType: "all",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
+
+export default mapWithoutLabels;
